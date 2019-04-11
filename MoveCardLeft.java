b49e4d86a305a25bb0eb8fca0329ff4e6014dfdb
MatthewChallen <48398806+MatthewChallen@users.noreply.github.com>
2019-04-11 15:04:18 +1000

    Update MoveCardLeft.java

--- MoveCardLeft.java
+++ MoveCardLeft.java
@@ -11,7 +11,6 @@ public class MoveCardLeft extends Card {
 	}
 	
 	public boolean play(PlayField theField) {
-		System.out.println("yo bro");
 		// Moves the player one space to the Left.
 		// Assumed a movePlayer() + getPlayer() method
 		// both in the PlayField class but can be changed
