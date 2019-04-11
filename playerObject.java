e6eeb432903dd6fe4b1b9a46f6131db8f7539c78
MatthewChallen <48398806+MatthewChallen@users.noreply.github.com>
2019-04-11 13:41:45 +1000

    Delete playerObject.java

--- playerObject.java
+++
@@ -1,9 +0,0 @@
-
-public class playerObject extends GameObjects {
-
-	public playerObject(int objectID, int xCoordinate, int yCoordinate) {
-		super(objectID, xCoordinate, yCoordinate);
-		// TODO Auto-generated constructor stub
-	}
-
-}
