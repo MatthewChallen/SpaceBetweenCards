cb262eeafe9bff901e910e63367db9fbb8e5fa11
Chris <moocj005@gmail.com>
2019-04-27 09:31:40 +0930

    Auto stash before merge of "master" and "origin/master"

---
+++ Card.java
@@ -0,0 +1,27 @@
+
+public abstract class Card {
+	private String name;
+	private String description;
+	
+	public Card(String name, String description) {
+		this.description = description;
+		this.name = name;
+	}
+	public boolean play(PlayField theField) {
+		// This method does the cards effect. Is overwritten for individual cards. Uses
+		// methods on the passed PlayField to do so. See PlayField for the methods.
+		// If the card cannot be played (it would move the player off the board, for
+		// example) then the method returns false. Otherwise, it returns true.
+		return false;
+	}
+	
+	public String getCardFileName() {
+		//This method returns a string of the filename of the JPG file that has the image of the card.
+		//This method is overwritten for individual cards.
+		return "strawberry.jpg";
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+}
